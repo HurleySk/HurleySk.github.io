da387b8caf7758fffb3b23c9b90e0b64b6ecc99c
HurleySk <sk.hurley@gmail.com>
2025-05-18 15:48:56 -0400

    Bird class

---
+++ cs_f001a/mod7/Bird.java
@@ -0,0 +1,61 @@
+package mod7;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Bird {
+    //Declare instance variables with defaults
+    private int id = -1;
+    private String type = "Unknown";
+
+    //Declare one static variable to keep track of previously used IDs and ensure uniqueness; another to determine next ID.
+    private static List<Integer> idsInUse = new ArrayList<>();
+    private static int nextId = 1;
+
+    //Constructor
+    public Bird(String type) {
+        //Set type to type passed in constructor
+        this.type = type;
+        
+        //Identify if nextId already in use to account for potential user-set IDs and increment up to next largest integer
+        while(idsInUse.contains(nextId)) {
+            nextId++;
+        }
+        
+        //Set ID to nextId then increment next nextId
+        this.id = nextId++;
+
+        //Add new ID to idsInUse
+        idsInUse.add(this.id);
+    }
+
+    //Getter for id instance variable
+    public int getId() {
+        return this.id;
+    }
+
+    //Setter for ID
+    public int setId(int newId) {
+        //If new ID below current nextId level, throw error
+        if (newId <= nextId) {
+            String errorMessage = "Please select an ID greater than " + nextId;
+            throw new IllegalArgumentException(errorMessage);
+        } else {
+            nextId = newId + 1;
+            this.id = newId;
+            return this.id;
+        }
+    }
+
+    //Getter for type
+    public String getType() {
+        return this.type;
+    }
+
+    //Setter for type
+    public String setType(String newType) {
+        this.type = newType;
+        return this.type;
+    }
+
+}
--- cs_f001a/mod7/Main.java
+++ cs_f001a/mod7/Main.java
@@ -1,5 +1,13 @@
+package mod7;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Bird bird0 = new Bird("Cardinal");
+        bird0.setId(5);
+        System.out.println(bird0.getId());
+
+        Bird bird1 = new Bird("Robin");
+        System.out.println(bird1.getId());
+        System.out.println(bird1.setId(3));
     }
 }
