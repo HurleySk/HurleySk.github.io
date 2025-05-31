947f1361461b202da721e6172d9f9d7f2be901ad
HurleySk <sk.hurley@gmail.com>
2025-05-31 13:24:06 -0400

    midterm work

---
+++ cs_f001a/midterm/MidtermScratchWork.java
@@ -0,0 +1,18 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MidtermScratchWork {
+    public static final int clubs = 0;
+    public static void main(String[] args) {
+        String myString = "testing123testing";
+        System.out.println(myString.substring(2, 6));
+    }
+}
+class world
+{    
+    public static void greetings()
+    {
+        System.out.println("Hola");   
+    }
+}
